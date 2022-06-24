5c19691a79652d84da2954b8793aca428cc9c93d
jhurd1 <jamiehurd3@gmail.com>
2022-06-23 21:11:58 -0600

    compiler doesn't recognize raw string

--- c_logHelp2/replaceDatString.cpp
+++ c_logHelp2/replaceDatString.cpp
@@ -14,7 +14,20 @@
 void ReplaceDatString::overwriteContent()
 {
   SearchLogic search;
-  FILE *pFile;
   std::string correspPath = search.getcorrespPath();
-  pFile = fopen(correspPath, "r+");//Get the file in question
+  std::string correspStrings = search.getcorrespStrings();
+  std::string stringInFile = search.getStringInFile();
+  std::string replacement = " **** ";
+  std::string line;
+  size_t len = stringInFile.length();
+  std::string regularExpression = R"\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b";
+  
+  do
+  {
+   search.searchLogic(correspPath, correspStrings, stringInFile);
+   
+  }
+  while(true);
+  
+  
 }
