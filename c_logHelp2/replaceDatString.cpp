ea196f5972f7344853ecca8ef44ff1fc41721ae7
jhurd1 <jamiehurd3@gmail.com>
2022-06-24 20:07:23 -0600

    data members added to replaceDatString

--- c_logHelp2/replaceDatString.cpp
+++ c_logHelp2/replaceDatString.cpp
@@ -8,26 +8,54 @@
 #include "replaceDatString.hpp"
 #include "searchLogic.hpp"
 #include <stdio.h>
+#include <regex>
 
-// should call searchLogic from here
-// revise searchDriver too
-void ReplaceDatString::overwriteContent()
+/*************************************************
+* DATA MEMBERS and
+* their respective mutators.
+***************************************************/
+SearchLogic searchLogic;
+
+std::string ReplaceDatString::getcorrespPath() const
 {
-  SearchLogic search;
-  std::string correspPath = search.getcorrespPath();
-  std::string correspStrings = search.getcorrespStrings();
-  std::string stringInFile = search.getStringInFile();
-  std::string replacement = " **** ";
+   return correspPath;
+}
+
+std::string ReplaceDatString::getReplacement() const
+{
+ return replacement;
+}
+
+void ReplaceDatString::setcorrespPath()
+{
+  this->correspPath = searchLogic.getcorrespPath();
+}
+
+void ReplaceDatString::setcorrespStrings()
+{
+  this->correspStrings = searchLogic.getcorrespStrings();
+}
+
+void ReplaceDatString::setstringInFile()
+{
+  this->stringInFile = searchLogic.getStringInFile();
+}
+
+void ReplaceDatString::setReplacement()
+{
+  this->replacement = " ******* ";
+}
+
+/****************************************
+FUNCTIONS and LOGIC
+ ****************************************/
+std::string ReplaceDatString::overwriteContent()
+{
   std::string line;
   size_t len = stringInFile.length();
-  std::string regularExpression = R"\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b";
+  std::regex r("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
   
-  do
-  {
-   search.searchLogic(correspPath, correspStrings, stringInFile);
-   
-  }
-  while(true);
   
+  return stringInFile;
   
 }
--- c_logHelp2/replaceDatString.hpp
+++ c_logHelp2/replaceDatString.hpp
@@ -8,13 +8,33 @@
 #ifndef replaceDatString_hpp
 #define replaceDatString_hpp
 #include <stdio.h>
+#include <string>
 
+/**********************************************
+* REPLACEDATSTRING
+* A class designed to evaluate whether
+* a found string embodies an IP or MAC
+* address and to overwrite such strings.
+**************************************************/
 class ReplaceDatString
 {
  private:
   FILE *pFile;
+  std::string correspPath;
+  std::string correspStrings;
+  std::string stringInFile;
+  std::string replacement;
+  std::string getcorrespPath() const;
+  std::string getcorrespStrings() const;
+  std::string getstringInFile() const;
+  std::string getReplacement() const;
+  void setcorrespPath();
+  void setcorrespStrings();
+  void setstringInFile();
+  void setReplacement();
+  
  public:
-  void overwriteContent();
+  std::string overwriteContent();
 };
 
 
