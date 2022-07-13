308ebb1ba5a5d9f4b6c03a8c0e457c3659a997a0
jhurd1 <jamiehurd3@gmail.com>
2022-07-13 17:51:21 -0600

    Swapping vec with arr to limit user input words

--- c_logHelp2/searchDriver.cpp
+++ c_logHelp2/searchDriver.cpp
@@ -11,6 +11,7 @@
 int main()
 {
     SearchDirs searchDirs;
+    std::string stop;
     std::string correspPath;
     std::string stringToFind;
    
@@ -18,7 +19,6 @@ int main()
  
     std::regex isnumber("^-?\\d+");
     std::regex ispath("/.*/+");
-    //std::regex ispath("/.*/.*/.*/");
     std::vector<std::string> temporary;
     std::smatch match;
     
@@ -26,22 +26,23 @@ int main()
     std::cin >> correspPath;
      if(std::regex_match(correspPath, ispath))
      {
-      std::cout << "You truly seek the holy grail. Therefore, you may pass.";
+      std::cout << "You truly seek the holy grail. Therefore, you may pass." << std::endl;
      } else
      {
       std::cout << "Not a path. You shall perish." << std::endl;
       return 1;
      }
     
-    std::cout << "Enter words to search, separated by a space: ";
-    while(std::getline(std::cin, stringToFind))
+    std::cout << "Enter up to three words, pressing enter between each. Enter '.' when through. " << std::endl;
+    while(std::getline(std::cin, stringToFind) && stop != ".")
     {
      if(std::regex_match(stringToFind, isnumber))
      {
-      std::cout << "Inappropriate string type entry." << std::endl;
+      std::cout << "Inappropriate data type for input." << std::endl;
       return 1;
      } else
      {
+      std::cin >> stringToFind;
       temporary.push_back(stringToFind);
      }
     }
